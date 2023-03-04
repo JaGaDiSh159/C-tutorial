aa881ca9067ee0253981f0a0474d689d08581bad
Jagadish <jagadishdash2004@gmail.com>
2023-03-04 16:15:53 +0530

    all commit

---
+++ 4comment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//this tells about comments
+/*
+multi line comment
+jkhdk
+*/
+int main(){
+    int age=22;
+    float pie=3.14;
+    char star='*';
+
+    return 0;
+}
---
+++ 5output.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int age = 22;
+    float pie = 3.14;
+    char star = '*';
+
+    printf ("my age is %d",age);
+    printf ("the value of pie is %f",pie) ;
+    printf ("star is %c", star);
+
+    return 0;
+}
